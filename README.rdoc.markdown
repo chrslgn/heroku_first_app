587ab579fe14954b17aa2e576e354f4bd6e9dc98
Chris Logan <chrs.lgn@gmail.com>
2012-04-13 01:53:06 -0400

    initial commit

---
+++ README.rdoc.markdown
@@ -0,0 +1,7 @@
+# Ruby on Rails Tutorial: first application
+# first_app
+
+This is the first application for 
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/) by [Michael Hartl](http://michaelhartl.com/).
+
+This application was written by [Chris Logan](chrs.lgn@gmail.com) by following and expounding upon the lessons in the above mentioned book.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ff16888c03168c80538abb6c255dbe9305c0b67464f4163627e17beecefef53b67eede4ad21a9e41bd4fe879be34af087206703abafad162bd86df709857d090'
